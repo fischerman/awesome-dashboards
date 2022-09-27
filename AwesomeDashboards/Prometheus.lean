dfedc66a17603a3cdf80ad89de212dc46eedf1e1
Björn Fischer <bjoern.fischer@inovex.de>
2022-09-27 14:00:26 +0200

    basic DSL and unit calculation

--- AwesomeDashboards/Prometheus.lean
+++ AwesomeDashboards/Prometheus.lean
@@ -10,6 +10,9 @@ inductive MetricUnit
 | seconds
 | bytes
 | bool
+| time
+| unitless
+deriving Repr, BEq, DecidableEq
 
 structure Metric where
 name : String
@@ -55,13 +58,11 @@ def name_label := "__name__"
 inductive InstantVector : InstantVectorType → Type
   | selector (literal_match : List KeyValuePair) (offset : Nat) : InstantVector vector -- TODO: regex, negative, and proof for minimal label requirements
   | literal (v : Float) : InstantVector InstantVectorType.scalar
-  | add_vector (vector_matching : Option VectorMatching) (a b : InstantVector scalar) : InstantVector scalar
+  | sub_vector (vector_matching : Option VectorMatching) (a b : InstantVector vector) : InstantVector scalar
   | add_scalar_left (a : InstantVector scalar) (b : InstantVector vector) : InstantVector vector
-  | gt (vector_matching : Option VectorMatching) (a b : InstantVector vector) : InstantVector vector
   | sum (a : AggregationSelector) (v : InstantVector vector) : InstantVector vector
   | topk (a : AggregationSelector) (k : Nat) (v : InstantVector vector) : InstantVector vector
   | rate (r : RangeVector) : InstantVector vector
-  | deriv (r : RangeVector) : InstantVector vector
   | label_replace (v : InstantVector vector) (dst replacement src regex : String) : InstantVector vector
   | time : InstantVector scalar 
 
@@ -75,8 +76,12 @@ def TypeSafeSelector (lm : List KeyValuePair) (e : Exporter) : Bool :=
 
 def InstantVector.typesafe {t : InstantVectorType} (v : InstantVector t) (e : Exporter) : Bool := match v with
   | (InstantVector.selector lm offset) => TypeSafeSelector lm e
-  | (InstantVector.add_vector _ a b) => typesafe a e && typesafe b e
+  | (InstantVector.sub_vector _ a b) => typesafe a e && typesafe b e
   | _ => true
+
+def InstantVector.withLiteralMatch {t : InstantVectorType} (key: String) (value: String) : InstantVector t → InstantVector t
+  | (InstantVector.selector lm offset) => InstantVector.selector (lm++[{key:=key, value:=value}]) offset
+  | x => x
 
 structure TypesafeInstantVector (t : InstantVectorType) (e : Exporter) where
 v : InstantVector t
@@ -97,31 +102,90 @@ instance {t : InstantVectorType} : Repr $ InstantVector t where
 
 #eval InstantVector.rate (RangeVector.selector [{key := "instance", value := "test"}] 5)
 
+def Exporter.unitOf (e : Exporter) (name : string) : Option MetricUnit := sorry
+
+def Option.get : (a : Option α) → a.isSome → α
+  | some a, _ => a
+
+def getNameLm? (lm : List KeyValuePair) : Option String :=
+  let name? := lm.find? (λl => is_name l)
+  match name? with
+    | (Option.some name) => name.value
+    | (Option.none) => Option.none
+
+def InstantVector.getName? {lm} {offset} (v : InstantVector vector) (h : v = InstantVector.selector lm offset) : Option String := 
+  getNameLm? lm
+
+def getMetricDef? (e : Exporter) (lm : List KeyValuePair) : Option Metric := match getNameLm? lm with
+  | (Option.some name) => e.metrics.find? (λm => m.name = name)
+  | (Option.none) => Option.none
+
+def unitOf {t : InstantVectorType} (e : Exporter) : InstantVector t → Option MetricUnit
+  | (InstantVector.time) => MetricUnit.time
+  | (InstantVector.selector lm offset) => match getMetricDef? e lm with
+    | (Option.some metric) => metric.unit
+    | _ => Option.none
+  | (InstantVector.label_replace v _ _ _ _) => unitOf e v
+  | (InstantVector.sub_vector _ a b) => if (unitOf e a) = (unitOf e b) then unitOf e a else Option.none
+  | (InstantVector.add_scalar_left  s v) => unitOf e v -- adding a scalar doesn't change the unit (however, it might no longer make sense)
+  | (InstantVector.rate r) => MetricUnit.seconds -- we need to implement unitOf for range vectors and allow units to be rates
+  | (InstantVector.literal f) => MetricUnit.unitless -- alternatively we could implement this function only vectors
+
 open Lean
 open Lean.Parser
+open Lean.PrettyPrinter
 
--- def name : Parser := withAntiquot (mkAntiquot "name" `LX.text) {
---   fn := fun c s => 
---     let startPos := s.pos;
---     let s := takeWhile1Fn (fun c => c.isAlphanum) "Invalid name" c s;
---     mkNodeToken `LX.text startPos c s
--- }
+def name : Parser := withAntiquot (mkAntiquot "name" `LX.text) {
+  fn := fun c s => 
+    let startPos := s.pos;
+    let s := takeWhile1Fn (fun c => c.isAlphanum || "_".contains c) "Invalid name" c s;
+    mkNodeToken `LX.text startPos c s
+}
 
+@[combinatorFormatter name] def name.formatter : Formatter := pure ()
+@[combinatorParenthesizer name] def name.parenthesizer : Parenthesizer := pure ()
+
+declare_syntax_cat labelmatcher_aux
+syntax name "=" strLit : labelmatcher_aux
+macro_rules
+| `(labelmatcher_aux| $key:name=$value) => `(InstantVector.selector [{key := $(quote key[0].getAtomVal!), value:= $value}] 0)
+
+-- 
 declare_syntax_cat labelmatcher
-syntax "l{" strLit,* "}" : labelmatcher
+-- The parser 
+syntax "{" labelmatcher_aux,* "}" : labelmatcher
+-- Make label matcher a valid term (not really neccessary)
 syntax labelmatcher : term
 
-syntax "pql!" strLit (labelmatcher)? : term
-
+-- How to translate label matchers into Lean terms
 macro_rules
-| `(labelmatcher| l{}) => `(List.nil)
-| `(labelmatcher| l{ $x }) => `([{key := $x, value := ""} : KeyValuePair])
---| `(labelmatcher| l{ $x, $xs,* }) => `([{key := $x, value := ""}])
+| `(labelmatcher| {}) => `(InstantVector.selector (@List.nil KeyValuePair) 0)
+| `(labelmatcher| { $x }) => `($x)
+--| `(labelmatcher| { $x, $xs,* }) => `({$xs,*})
 
+syntax "[pql|" name (labelmatcher)? "]" : term
+syntax "[pql|" "time()" "]" : term
+
+set_option trace.Elab.definition true in
 macro_rules
-| `(pql! $name) => `(InstantVector.selector [{key := "__name__", value:= $(name)}] 0)
-| `(pql! $name $xs) => `(InstantVector.selector ([{key := "__name__", value:= $(name)}]++($xs)) 0)
+| `([pql| time()]) => `(InstantVector.time)
+| `([pql| $name]) => `(InstantVector.selector [{key := "__name__", value:= $(quote name[0].getAtomVal!)}] 0)
+| `([pql| $name $xs]) => `(InstantVector.withLiteralMatch "__name__" $(quote name[0].getAtomVal!)  $xs)
 
-#eval pql!"upz"l{}
+#eval InstantVector.selector ([{key := "__name__", value:= "f"}]++[{key := "$x", value := ""}]) 0
+set_option pp.rawOnError true
+#eval [pql| up{}]
+#eval [pql| up{instance="localhost"}]
+-- #eval [pql| up{test="abc", lan="def"}]
+#eval [pql| time()]
+
 
 #eval s!"f"
+
+-- syntax:max can be used to change precendense
+
+syntax "`[abc| " name "]" : term
+macro_rules
+| `(`[abc| $x ]) => pure $ quote ("<" ++ x[0].getAtomVal! ++ "/>")
+
+#check `[abc| def]
